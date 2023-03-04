0a932e847a0f5c29ca10276f679cf0916dd15dc8
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-03-04 13:22:33 +0000

    Add nb_eval for notebooks testing

---
+++ tests/nb_eval.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from time import time
+
+import click
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+workspace = Path(__file__).parents[0] / "workspace"
+workspace.mkdir(parents=True, exist_ok=True)
+
+
+def run_notebook(notebook_path: Path) -> None:
+    with open(notebook_path, encoding="utf8") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=1800)
+    # Will raise on cell error:
+    proc.preprocess(nb, {"metadata": {"path": workspace}})
+
+
+# For fine-grained control of which notebooks to test, adjust this:
+enabled_tests = [
+    "tutorial",  # All tutorial notebooks include "tutorial" in file name.
+]
+
+
+@click.command()
+@click.option("--nb_dir", type=str, default=".")
+def main(nb_dir: Path) -> None:
+    nb_dir = Path(nb_dir)
+
+    for p in nb_dir.rglob("*"):
+        if p.suffix != ".ipynb":
+            continue
+        if "checkpoint" in p.name:
+            continue
+
+        ignore = True
+        for val in enabled_tests:
+            if val in p.name:
+                ignore = False
+                break
+        if ignore:
+            continue
+
+        print("Testing notebook:", p.name)
+        start = time()
+        try:
+            run_notebook(p)
+        except BaseException as e:
+            print("FAIL", p.name, e)
+            raise e
+        finally:
+            print(f"Notebook {p.name} took {time() - start:.2f} sec")
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
