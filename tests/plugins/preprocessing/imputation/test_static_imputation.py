fc59b12d5eafb3a388f2e30677b0a60218aca780
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-03-15 16:32:32 +0000

    add static imputer

---
+++ tests/plugins/preprocessing/imputation/test_static_imputation.py
@@ -0,0 +1,47 @@
+import pytest
+
+from tempor.plugins import plugin_loader
+from tempor.plugins.preprocessing.imputation import BaseImputer
+from tempor.plugins.preprocessing.imputation.plugin_static_imputation import (
+    StaticOnlyImputer as plugin,
+)
+from tempor.utils.datasets.sine import SineDataloader
+
+
+def from_api() -> BaseImputer:
+    return plugin_loader.get("preprocessing.imputation.static_imputation", random_state=123)
+
+
+def from_module() -> BaseImputer:
+    return plugin(random_state=123)
+
+
+@pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
+def test_static_imputation_plugin_sanity(test_plugin: BaseImputer) -> None:
+    assert test_plugin is not None
+    assert len(test_plugin.hyperparameter_space()) == 2
+
+
+@pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
+def test_static_imputation_plugin_fit(test_plugin: BaseImputer) -> None:
+    dataset = SineDataloader(with_missing=True).load()
+    assert dataset.static.dataframe().isna().sum().sum() != 0
+
+    test_plugin.fit(dataset)
+
+
+@pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
+def test_static_imputation_plugin_transform(test_plugin: BaseImputer) -> None:
+    dataset = SineDataloader(with_missing=True).load()
+    assert dataset.static.dataframe().isna().sum().sum() != 0
+
+    output = test_plugin.fit(dataset).transform(dataset)
+
+    assert output.static.dataframe().isna().sum().sum() == 0
+    assert output.time_series.dataframe().isna().sum().sum() == 0
+
+
+def test_hyperparam_sample():
+    for repeat in range(100):
+        args = plugin._cls.sample_hyperparameters()
+        plugin(**args)
