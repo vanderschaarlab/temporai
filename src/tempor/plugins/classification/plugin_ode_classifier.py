6d79c8573b0e35a4afa717f284dba329ec9437f0
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-03-16 20:31:41 +0000

    ODE clf

--- src/tempor/plugins/classification/plugin_ode_classifier.py
+++ src/tempor/plugins/classification/plugin_ode_classifier.py
@@ -2,57 +2,61 @@ from typing import Any, Optional
 
 import numpy as np
 from torch.utils.data import sampler
-from typing_extensions import get_args
 
 import tempor.plugins.core as plugins
 from tempor.data import dataset, samples
 from tempor.models.constants import DEVICE
-from tempor.models.ts_model import Nonlin, TimeSeriesModel, TSModelMode
+from tempor.models.ts_ode import NeuralODE, Nonlin
 from tempor.plugins.classification import BaseClassifier
 from tempor.plugins.core._params import CategoricalParams, FloatParams, IntegerParams
 
 
-@plugins.register_plugin(name="nn_classifier", category="classification")
-class NeuralNetClassifier(BaseClassifier):
+@plugins.register_plugin(name="ode_classifier", category="classification")
+class ODEClassifier(BaseClassifier):
     def __init__(
         self,
         *,
-        n_static_units_hidden: int = 100,
-        n_static_layers_hidden: int = 2,
-        n_temporal_units_hidden: int = 102,
-        n_temporal_layers_hidden: int = 2,
-        n_iter: int = 500,
-        mode: TSModelMode = "RNN",
-        n_iter_print: int = 10,
-        batch_size: int = 100,
+        n_units_hidden: int = 100,
+        n_layers_hidden: int = 1,
+        nonlin: Nonlin = "relu",
+        dropout: float = 0,
+        # ODE specific
+        atol: float = 1e-2,
+        rtol: float = 1e-2,
+        interpolation: str = "cubic",
+        # training
         lr: float = 1e-3,
         weight_decay: float = 1e-3,
-        window_size: int = 1,
+        opt_betas: tuple = (0.9, 0.999),
+        n_iter: int = 1000,
+        batch_size: int = 500,
+        n_iter_print: int = 100,
+        random_state: int = 0,
+        patience: int = 10,
+        n_iter_min: int = 100,
+        clipping_value: int = 1,
+        train_ratio: float = 0.8,
         device: Any = DEVICE,
         dataloader_sampler: Optional[sampler.Sampler] = None,
-        dropout: float = 0,
-        nonlin: Nonlin = "relu",
-        random_state: int = 0,
-        clipping_value: int = 1,
-        patience: int = 20,
-        train_ratio: float = 0.8,
     ) -> None:
-        """Neural-net classifier.
+        """Classifier based on ordinary differential equation (ODE) solvers.
+
 
         Args:
-            n_static_units_hidden (int, optional):
-                Number of hidden units for the static features. Defaults to ``100``.
-            n_static_layers_hidden (int, optional):
-                Number of hidden layers for the static features. Defaults to ``2``.
-            n_temporal_units_hidden (int, optional):
-                Number of hidden units for the temporal features. Defaults to ``102``.
-            n_temporal_layers_hidden (int, optional):
-                Number of hidden layers for the temporal features. Defaults to ``2``.
+            n_units_hidden (int, optional):
+                Number of hidden units. Defaults to ``100``.
+            n_layers_hidden (int, optional):
+                Number of hidden layers. Defaults to ``2``.
+            # ODE specific
+            atol: float
+                absolute tolerance for solution
+            rtol: float
+                relative tolerance for solution
+            interpolation: str
+                cubic or linear
+            # training
             n_iter (int, optional):
                 Number of epochs. Defaults to ``500``.
-            mode (TSModelMode, optional):
-                Core neural net architecture. Available options: :obj:`~tempor.models.ts_model.TSModelMode`.
-                Defaults to ``"RNN"``.
             n_iter_print (int, optional):
                 Number of epochs to print the loss. Defaults to ``10``.
             batch_size (int, optional):
@@ -61,8 +65,6 @@ class NeuralNetClassifier(BaseClassifier):
                 Learning rate. Defaults to ``1e-3``.
             weight_decay (float, optional):
                 l2 (ridge) penalty for the weights. Defaults to ``1e-3``.
-            window_size (int, optional):
-                How many hidden states to use for the outcome. Defaults to ``1``.
             device (Any, optional):
                 PyTorch device to use. Defaults to `~tempor.models.constants.DEVICE`.
             dataloader_sampler (Optional[sampler.Sampler], optional):
@@ -82,17 +84,13 @@ class NeuralNetClassifier(BaseClassifier):
                 Train/test split ratio. Defaults to ``0.8``.
         """
         super().__init__()
-        self.n_static_units_hidden = n_static_units_hidden
-        self.n_static_layers_hidden = n_static_layers_hidden
-        self.n_temporal_units_hidden = n_temporal_units_hidden
-        self.n_temporal_layers_hidden = n_temporal_layers_hidden
+        self.n_units_hidden = n_units_hidden
+        self.n_layers_hidden = n_layers_hidden
         self.n_iter = n_iter
-        self.mode: TSModelMode = mode
         self.n_iter_print = n_iter_print
         self.batch_size = batch_size
         self.lr = lr
         self.weight_decay = weight_decay
-        self.window_size = window_size
         self.device = device
         self.dataloader_sampler = dataloader_sampler
         self.dropout = dropout
@@ -102,36 +100,42 @@ class NeuralNetClassifier(BaseClassifier):
         self.patience = patience
         self.train_ratio = train_ratio
 
-        self.model: Optional[TimeSeriesModel] = None
+        # ODE
+        self.atol = atol
+        self.rtol = rtol
+        self.interpolation = interpolation
+
+        self.model: Optional[NeuralODE] = None
 
     def _fit(
         self,
         data: dataset.Dataset,
         *args,
         **kwargs,
-    ) -> "NeuralNetClassifier":  # pyright: ignore
+    ) -> "ODEClassifier":  # pyright: ignore
         static, temporal, observation_times, outcome = self._unpack_dataset(data)
         outcome = outcome.squeeze()
 
         n_classes = len(np.unique(outcome))
 
-        self.model = TimeSeriesModel(
+        self.model = NeuralODE(
             task_type="classification",
             n_static_units_in=static.shape[-1],
             n_temporal_units_in=temporal.shape[-1],
-            n_temporal_window=temporal.shape[1],
             output_shape=[n_classes],
-            n_static_units_hidden=self.n_static_units_hidden,
-            n_static_layers_hidden=self.n_static_layers_hidden,
-            n_temporal_units_hidden=self.n_temporal_units_hidden,
-            n_temporal_layers_hidden=self.n_temporal_layers_hidden,
+            n_units_hidden=self.n_units_hidden,
+            n_layers_hidden=self.n_layers_hidden,
+            # ODE
+            backend="ode",
+            atol=self.atol,
+            rtol=self.rtol,
+            interpolation=self.interpolation,
+            # training
             n_iter=self.n_iter,
-            mode=self.mode,
             n_iter_print=self.n_iter_print,
             batch_size=self.batch_size,
             lr=self.lr,
             weight_decay=self.weight_decay,
-            window_size=self.window_size,
             device=self.device,
             dataloader_sampler=self.dataloader_sampler,
             dropout=self.dropout,
@@ -179,11 +183,11 @@ class NeuralNetClassifier(BaseClassifier):
     @staticmethod
     def hyperparameter_space(*args, **kwargs):
         return [
-            IntegerParams(name="n_static_units_hidden", low=100, high=1000),
-            IntegerParams(name="n_static_layers_hidden", low=1, high=5),
-            IntegerParams(name="n_temporal_units_hidden", low=100, high=1000),
-            IntegerParams(name="n_temporal_layers_hidden", low=1, high=5),
-            CategoricalParams(name="mode", choices=list(get_args(TSModelMode))),
+            IntegerParams(name="n_units_hidden", low=100, high=1000),
+            IntegerParams(name="n_layers_hidden", low=1, high=5),
+            FloatParams(name="atol", low=0, high=1),
+            FloatParams(name="rtol", low=0, high=1),
+            CategoricalParams(name="interpolation", choices=["cubic", "linear"]),
             CategoricalParams(name="batch_size", choices=[64, 128, 256, 512]),
             CategoricalParams(name="lr", choices=[1e-3, 1e-4, 2e-4]),
             FloatParams(name="dropout", low=0, high=0.2),
