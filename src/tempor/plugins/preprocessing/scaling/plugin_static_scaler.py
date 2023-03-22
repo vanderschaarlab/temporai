504789e00a484758a8cc3486140e9b560ba3facd
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-03-22 07:47:47 +0000

    more scalers

---
+++ src/tempor/plugins/preprocessing/scaling/plugin_static_scaler.py
@@ -0,0 +1,64 @@
+from typing import Any
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from typing_extensions import Self
+
+import tempor.plugins.core as plugins
+from tempor.data import dataset
+from tempor.data.samples import StaticSamples
+from tempor.plugins.preprocessing.scaling import BaseScaler
+
+
+@plugins.register_plugin(name="static_scaler", category="preprocessing.scaling")
+class StaticScalerScaler(BaseScaler):
+    def __init__(self, **params) -> None:  # pylint: disable=useless-super-delegation
+        """MinMax scaling for the static data.
+
+        Example:
+            >>> from tempor.utils.datasets.sine import SineDataloader
+            >>> from tempor.plugins import plugin_loader
+            >>>
+            >>> dataset = SineDataloader().load()
+            >>>
+            >>> # Load the model:
+            >>> model = plugin_loader.get("preprocessing.scaling.static_scaler")
+            >>>
+            >>> # Train:
+            >>> model.fit(dataset)
+            >>>
+            >>> # Scale:
+            >>> scaled = model.transform(dataset)
+        """
+
+        super().__init__(**params)
+        self.model = StandardScaler()
+
+    def _fit(
+        self,
+        data: dataset.Dataset,
+        *args,
+        **kwargs,
+    ) -> Self:
+        if data.static is None:
+            return self
+
+        self.model.fit(data.static.dataframe())
+        return self
+
+    def _transform(self, data: dataset.Dataset, *args, **kwargs) -> Any:
+        if data.static is None:
+            return data
+
+        static_data = data.static.dataframe()
+        scaled = pd.DataFrame(self.model.transform(static_data))
+        scaled.columns = static_data.columns
+        scaled.index = static_data.index
+
+        data.static = StaticSamples.from_dataframe(scaled)
+
+        return data
+
+    @staticmethod
+    def hyperparameter_space(*args, **kwargs):
+        return []
--- tests/plugins/preprocessing/scaling/test_static_scaler.py
+++ tests/plugins/preprocessing/scaling/test_static_scaler.py
@@ -4,14 +4,14 @@ import pytest
 
 from tempor.plugins import plugin_loader
 from tempor.plugins.preprocessing.scaling import BaseScaler
-from tempor.plugins.preprocessing.scaling.plugin_static_minmax_scaler import (
-    StaticMinMaxScalerScaler as plugin,
+from tempor.plugins.preprocessing.scaling.plugin_static_scaler import (
+    StaticScalerScaler as plugin,
 )
 from tempor.utils.dataloaders import GoogleStocksDataLoader, SineDataLoader
 
 
 def from_api() -> BaseScaler:
-    return plugin_loader.get("preprocessing.scaling.static_minmax_scaler", random_state=123)
+    return plugin_loader.get("preprocessing.scaling.static_scaler", random_state=123)
 
 
 def from_module() -> BaseScaler:
@@ -19,29 +19,29 @@ def from_module() -> BaseScaler:
 
 
 @pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
-def test_static_minmax_scaler_plugin_sanity(test_plugin: BaseScaler) -> None:
+def test_static_scaler_plugin_sanity(test_plugin: BaseScaler) -> None:
     assert test_plugin is not None
-    assert test_plugin.name == "static_minmax_scaler"
+    assert test_plugin.name == "static_scaler"
     assert len(test_plugin.hyperparameter_space()) == 0
 
 
 @pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
 @pytest.mark.parametrize("dataloader", [GoogleStocksDataLoader(), SineDataLoader(static_scale=5)])
-def test_static_minmax_scaler_plugin_fit(test_plugin: BaseScaler, dataloader: Any) -> None:
+def test_static_scaler_plugin_fit(test_plugin: BaseScaler, dataloader: Any) -> None:
     dataset = dataloader.load()
     test_plugin.fit(dataset)
 
 
 @pytest.mark.parametrize("test_plugin", [from_api(), from_module()])
-def test_static_minmax_scaler_plugin_transform(test_plugin: BaseScaler) -> None:
+def test_static_scaler_plugin_transform(test_plugin: BaseScaler) -> None:
     dataset = SineDataLoader(static_scale=100).load()
     assert dataset.static is not None  # nosec B101
-    assert (dataset.static.numpy() > 1.1).any()
+    assert (dataset.static.numpy() > 50).any()
 
     output = test_plugin.fit(dataset).transform(dataset)
 
-    assert (output.static.numpy() < 1 + 1e-1).all()
-    assert (output.static.numpy() >= 0).all()
+    print(output.static.numpy().min(), output.static.numpy().max())
+    assert (output.static.numpy() < 50).all()
 
 
 def test_hyperparam_sample():
