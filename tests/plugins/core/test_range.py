44d768ece0f5098da165244a439bbc235354a1ff
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-03-30 16:53:25 +0100

    Tidy tests/plugins/core/

--- tests/plugins/core/test_range.py
+++
@@ -1,9 +0,0 @@
-from tempor.plugins.preprocessing.scaling.plugin_static_minmax_scaler import (
-    StaticMinMaxScaler as plugin,
-)
-
-
-def test_hyperparam_sample():
-    for repeat in range(10000):  # pylint: disable=unused-variable
-        args = plugin.sample_hyperparameters()
-        plugin(**args)
