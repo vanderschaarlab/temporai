081b7acc89bf6eb9e575e528cdbc1ecb7373fe74
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-03-30 16:27:13 +0100

    Type hints for better static type-checker compat.

--- src/tempor/plugins/core/_plugin.py
+++ src/tempor/plugins/core/_plugin.py
@@ -5,7 +5,9 @@ import importlib.util
 import os
 import os.path
 import sys
-from typing import Any, ClassVar, Dict, List, Type
+from typing import TYPE_CHECKING, Any, Callable, ClassVar, Dict, List, Type, TypeVar
+
+from typing_extensions import ParamSpec
 
 import tempor
 from tempor.log import logger
@@ -14,6 +16,9 @@ from . import utils
 
 PLUGIN_NAME_NOT_SET = "NOT_SET"
 PLUGIN_CATEGORY_NOT_SET = "NOT_SET"
+
+P = ParamSpec("P")
+T = TypeVar("T")
 
 
 class Plugin:
