fdad0fc663a6c79c214dcb52c43dc9eead7741e6
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-04-02 18:08:13 +0100

    Move imports inside fixtures in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,6 @@ from typing import TYPE_CHECKING, Any, List, Tuple
 
 import numpy as np
 import pytest
-from clairvoyance2.datasets.dummy import dummy_dataset
-from clairvoyance2.datasets.simulated.simple_pkpd import simple_pkpd_dataset
 
 if TYPE_CHECKING:
     from tempor.data import dataset
