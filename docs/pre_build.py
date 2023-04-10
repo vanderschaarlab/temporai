b7b259db5e7e7f62d230015a898405fd753f33c5
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-04-10 19:46:54 +0100

    Update pre_build.py to make a PyPI README

--- docs/pre_build.py
+++ docs/pre_build.py
@@ -3,12 +3,14 @@
 The code will:
 * Update ``docs/requirements.txt`` with the up-to-date requirements from the library.
 * Update ``docs/overview.md`` based on the main ``README.md`` (carries out some string substitutions etc.).
+* Create the PyPI version of ``README.md``, ``pypi.md``, with image links fixed.
 """
 
 import os
 import re
 
 REPO_URL = "https://github.com/vanderschaarlab/temporai/"
+
 
 # -- Update `docs/requirements.txt` with the content of `install_requires` in `setup.cfg`.
 
@@ -32,10 +34,12 @@ with open(DOCS_REQS_FILE, "w", encoding="utf8") as file:
     file.write(docs_req_content)
 
 # -- Convert `README.md` into `overview.md`.
+
 README_PATH = os.path.join(os.path.dirname(__file__), "../README.md")
 OVERVIEW_PATH = os.path.join(os.path.dirname(__file__), "overview.md")
 
 REPLACE = {
+    # Add more as necessary.
     "[User Guide][docs/user_guide]": "[User Guide](user_guide/index)",
     "<!-- include_docs": "",
     "include_docs_end -->": "",
@@ -69,4 +73,32 @@ readme_content = re.sub(
 # readme_content = re.sub(r"\:[a-z1-9+\-_]{0,100}\:", (lambda x: f"|{x.group(0)}|"), readme_content)
 
 with open(OVERVIEW_PATH, "w", encoding="utf8") as file:
+    file.write(readme_content)
+
+
+# -- Convert `README.md` into `pypi.md`.
+
+PYPI_README_PATH = os.path.join(os.path.dirname(__file__), "../pypi.md")
+
+REPLACE = {
+    # Add more as necessary.
+    "./": REPO_URL,
+}
+
+with open(README_PATH, "r", encoding="utf8") as file:
+    readme_content = file.read()
+
+# Replace:
+for k, v in REPLACE.items():
+    readme_content = readme_content.replace(k, v)
+
+# Fix images:
+convert = {
+    r"\"docs/assets/(.*?\..*?)\"": r"'https://raw.githubusercontent.com/vanderschaarlab/temporai/main/docs/assets/\1'",
+    r"\[docs/assets/(.*?\..*?)\]": r"[https://raw.githubusercontent.com/vanderschaarlab/temporai/main/docs/assets/\1]",
+}
+for source, destination in convert.items():
+    readme_content = re.sub(source, destination, readme_content, flags=re.DOTALL)
+
+with open(PYPI_README_PATH, "w", encoding="utf8") as file:
     file.write(readme_content)
