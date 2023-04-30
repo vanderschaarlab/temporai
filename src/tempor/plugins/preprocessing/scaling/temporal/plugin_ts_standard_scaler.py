a63c506f2d05949655dcb6c5cf9ad8a052a830a3
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-04-30 19:46:27 +0100

    Register scalers' input parameters

--- src/tempor/plugins/preprocessing/scaling/temporal/plugin_ts_standard_scaler.py
+++ src/tempor/plugins/preprocessing/scaling/temporal/plugin_ts_standard_scaler.py
@@ -1,3 +1,6 @@
+import dataclasses
+from typing import Any, Dict
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from typing_extensions import Self
@@ -8,12 +11,31 @@ from tempor.data.samples import TimeSeriesSamples
 from tempor.plugins.preprocessing.scaling._base import BaseScaler
 
 
+@dataclasses.dataclass
+class TimeSeriesStandardScalerParams:
+    """Initialization parameters for :class:`TimeSeriesStandardScaler`."""
+
+    with_mean: bool = True
+    """If True, center the data before scaling. See `sklearn.preprocessing.StandardScaler`."""
+    with_std: bool = True
+    """If True, scale the data to unit variance. See `sklearn.preprocessing.StandardScaler`."""
+
+
 @plugins.register_plugin(name="ts_standard_scaler", category="preprocessing.scaling.temporal")
 class TimeSeriesStandardScaler(BaseScaler):
-    def __init__(self, **params) -> None:  # pylint: disable=useless-super-delegation
+    ParamsDefinition = TimeSeriesStandardScalerParams
+    params: TimeSeriesStandardScalerParams  # type: ignore
+
+    def __init__(self, **params) -> None:
         """Standard scaling for the time-series data.
 
-        Standardize the temporal features by removing the mean and scaling to unit variance.
+        Standardize the temporal features by removing the mean and scaling to unit variance. The time series data
+        will be represented as a multi-index `(sample_idx, time_idx)` dataframe of features, and the scaling will be
+        applied to this dataframe.
+
+        Args:
+            **params:
+                Parameters and defaults as defined in :class:`TimeSeriesStandardScalerParams`.
 
         Example:
             >>> from tempor.utils.dataloaders import SineDataLoader
@@ -31,9 +53,9 @@ class TimeSeriesStandardScaler(BaseScaler):
             >>> # Scale:
             >>> scaled = model.transform(dataset)
         """
-
         super().__init__(**params)
-        self.model = StandardScaler()
+        sklearn_params: Dict[str, Any] = dict(self.params)  # type: ignore
+        self.model = StandardScaler(**sklearn_params)
 
     def _fit(
         self,
