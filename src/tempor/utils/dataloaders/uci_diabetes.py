b8fc671f2e45a4b6ae49d492831c8b57f0a226e4
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-05-03 14:26:31 +0100

    Add download retries for UCI diabetes dataset

--- src/tempor/utils/dataloaders/uci_diabetes.py
+++ src/tempor/utils/dataloaders/uci_diabetes.py
@@ -1,11 +1,16 @@
+import time
+import traceback
+import urllib.error
 from pathlib import Path
 from typing import Tuple, cast
 
+import requests
 from clairvoyance2.datasets.uci import uci_diabetes
 from clairvoyance2.preprocessing.convenience import TemporalTargetsExtractor
 
 from tempor.data import dataloader, dataset
 from tempor.data.clv2conv import clairvoyance2_dataset_to_tempor_dataset
+from tempor.log import logger
 
 
 # TODO: Docstring.
@@ -31,14 +36,32 @@ class UCIDiabetesDataLoader(dataloader.TemporalPredictionDataLoader):
         return str(Path(UCIDiabetesDataLoader.data_root_dir) / "uci_diabetes")
 
     def load(self, **kwargs) -> dataset.TemporalPredictionDataset:
-        clv_dataset = uci_diabetes(
-            data_home=UCIDiabetesDataLoader.data_root_dir,
-            refresh_cache=True,
-            redownload=False,
-            make_regular=self.make_regular,
-            use_int_index=self.use_int_index,
+        download_retries = 3
+        download_pause_sec = 5
+        for retry in range(download_retries):
+            # NOTE: Connection to archive.ics.uci.edu tends to be flaky, attempt download retries.
+            # TODO: May wish to exclude this from tests / download files from a more stable location /
+            # make tests resilient to internet connection failures.
+            try:
+                clv_dataset = uci_diabetes(
+                    data_home=UCIDiabetesDataLoader.data_root_dir,
+                    refresh_cache=True,
+                    redownload=False,
+                    make_regular=self.make_regular,
+                    use_int_index=self.use_int_index,
+                )
+            except (requests.exceptions.RequestException, urllib.error.URLError) as ex:
+                if retry + 1 == download_retries:
+                    logger.error(f"Failed to download UCI diabetes dataset after {download_retries} retries.")
+                    raise
+                logger.debug(
+                    f"Caught exception and will retry ({retry + 1}/{download_retries}): "
+                    f"{ex}\n{traceback.format_exc()}"
+                )
+                time.sleep(download_pause_sec)
+        clv_dataset = TemporalTargetsExtractor(params={"targets": self.targets}).fit_transform(
+            clv_dataset  # pyright: ignore
         )
-        clv_dataset = TemporalTargetsExtractor(params={"targets": self.targets}).fit_transform(clv_dataset)
         data = clairvoyance2_dataset_to_tempor_dataset(clv_dataset)
         data = cast(dataset.TemporalPredictionDataset, data)
         return data
