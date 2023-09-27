ee45a604e7a181b1b12af62040c818ad90021af9
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-09-27 12:28:35 +0100

    Update how docs reqs are managed

--- docs/pre_build.py
+++ docs/pre_build.py
@@ -1,7 +1,6 @@
 """A docs building code that must be ran when building TemporAI docs.
 
 The code will:
-* Update ``docs/requirements.txt`` with the up-to-date requirements from the library.
 * Update ``docs/overview.md`` based on the main ``README.md`` (carries out some string substitutions etc.).
 * Create the PyPI version of ``README.md``, ``pypi.md``, with image links fixed.
 """
@@ -13,31 +12,6 @@ from pre_build_nbconvert import do_conversion
 
 REPO_URL_ROOT = "https://github.com/vanderschaarlab/temporai/"
 REPO_URL_TREE = f"{REPO_URL_ROOT}tree/main/"
-
-
-# -- Update `docs/requirements.txt` with the content of `install_requires` in `setup.cfg`.
-
-print("Updating `docs/requirements.txt`...")
-
-SETUP_CFG_PATH = os.path.join(os.path.dirname(__file__), "../setup.cfg")
-DOCS_REQS_FILE = os.path.join(os.path.dirname(__file__), "requirements.txt")
-with open(SETUP_CFG_PATH, "r", encoding="utf8") as file:
-    setup_cfg_content = file.read()
-final_specs = ""
-for name, block in zip(("install_requires", "dev"), (r"install_requires\s=.*?\n\n", r"dev\s=.*?\n\n")):
-    found = re.findall(block, setup_cfg_content, re.DOTALL)
-    list_dep_specs = found[0].split("\n    ")[1:]
-    list_dep_specs[-1] = list_dep_specs[-1].replace("\n", "")
-    list_dep_specs = [dep for dep in list_dep_specs if dep[0] != "#"]  # Remove comment lines.
-    final_specs += f"# {name}:\n" + "\n".join(sorted(list_dep_specs)) + "\n"
-with open(DOCS_REQS_FILE, "r", encoding="utf8") as file:
-    docs_req_content = file.read()
-found = re.findall(r"# ----- auto_update -----.*# ----- auto_update -----", docs_req_content, re.DOTALL)[0]
-new = f"# ----- auto_update -----\n{final_specs}# ----- auto_update -----"
-docs_req_content = docs_req_content.replace(found, new)  # type: ignore
-
-with open(DOCS_REQS_FILE, "w", encoding="utf8") as file:
-    file.write(docs_req_content)
 
 # -- Convert `README.md` into `overview.md`.
 print("Working on `docs/overview.md`...")
